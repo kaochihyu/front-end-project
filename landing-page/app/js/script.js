0626098502b925080c225446bf0956a4e84d09d1
kaochihyu <k2307874@gmail.com>
2021-04-30 21:05:31 +0800

    landing page

---
+++ landing-page/app/js/script.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() { 
+  console.log('open hamburger');
+
+  if (header.classList.contains('open')) { // Close hamburger menu
+    header.classList.remove('open');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out'); 
+    })
+  } else { // Open hamburger menu
+    header.classList.add('open');
+    body.classList.add('noscroll')
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
+  }
+})
+
